66460593499a444cc601bd87275a1e93225783e6
harshit-ttn <harshit.singhal@tothenew.com>
2024-09-25 10:31:19 +0530

    videoplayer file changes

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # AngularVideoJSPlayerWithWebpack
+
 Videoplayer app with webpack setup in angular.js in which we can play DRM, Non-DRM and Live Contents
 
-* Here we are going to use video.js player in our project for playing the contents
+- Here we are going to use video.js player in our project for playing the contents
 
 # Install the Angular CLI
+
 npm install -g @angular/cli@17
 
 # Create a workspace and initial application
+
 ng new my-app
 
 # Run the application
+
 cd my-app
 ng serve --open
+
+# Blog Link
+
+[Blog on AngularJS](https://medium.com/@harshitsinghal641/implementing-video-js-in-angularjs-for-drm-non-drm-and-live-contents-68a47475111a)
--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit, ElementRef, ViewChild, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  Input,
+  OnDestroy,
+} from '@angular/core';
 import videojs from 'video.js';
 import { CommonModule } from '@angular/common';
+import { drmDataList } from '../../utils/drmData';
+import { nonDrmDataList } from '../../utils/nonDrmData';
+import { liveDataList } from '../../utils/liveData';
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
@@ -8,23 +18,44 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
 })
-export class VideoPlayerComponent implements OnInit {
+export class VideoPlayerComponent implements OnInit, OnDestroy {
   @ViewChild('videoPlayer', { static: true }) videoPlayer!: ElementRef;
   player: any;
   @Input() index: any;
   @Input() contentType: any;
 
   ngOnInit(): void {
-    this.player = videojs(this.videoPlayer.nativeElement, {
-      controls: false,
-      autoplay: true,
-      preload: 'auto',
-      sources: [
-        {
-          src: 'https://example.com/video.m3u8',
-          type: 'application/x-mpegURL',
+    this.initializePlayer();
+  }
+
+  initializePlayer(): void {
+    let videoSource: any = {
+      src: '',
+      type: 'application/x-mpegURL',
+    };
+
+    const drmOptions = {
+      drm: {
+        widevine: {
+          licenseUrl: 'https://example.com/widevine-license-url',
         },
-      ],
+      },
+    };
+
+    if (this.contentType === 'drm') {
+      videoSource.src = ``;
+      videoSource.keySystems = drmOptions.drm; // Add DRM configuration
+    } else if (this.contentType === 'non-drm') {
+      videoSource.src = ``;
+    } else if (this.contentType === 'live') {
+      videoSource.src = ``;
+    }
+
+    this.player = videojs(this.videoPlayer.nativeElement, {
+      controls: true,
+      autoplay: false,
+      preload: 'auto',
+      sources: [videoSource],
     });
   }
 
